10664b37045fc554ff2af1ce2abaae84f946ce66
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-10-27 15:23:11 -0400

    Eglinton Square 2.0 1.4  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Eglinton Square",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f503036e6f6454f2010000/image/jpeg/undefined/lbtc_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//longbeach.mallmaverick.com/api/v2/longbeach/social.json"
+                }
